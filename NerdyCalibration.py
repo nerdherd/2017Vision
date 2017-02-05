06099fe9441dfb6f89f0f09dea7bf7ccf28ec144
tedfoodlin <tedklin@gmail.com>
2017-02-04 19:31:44 -0800

    update calibration box display

--- NerdyCalibration.py
+++ NerdyCalibration.py
@@ -3,11 +3,11 @@ import numpy as np
 import math
 
 """FRC Vision Target Calibration"""
-__author__ = "tedfoodlin"
+__author__ = "Ted Lin"
 
 # Capture video from camera
 # 0 for laptop webcam, 1 for USB camera connected to laptop, 0 for camera on RPI
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 # Specs in use (Microsoft Lifecam HD-3000)
 FRAME_X = 640
@@ -18,7 +18,7 @@ FRAME_CX = 320
 FRAME_CY = 240
 
 # Calibration box dimensions
-CAL_AREA = 1600
+CAL_AREA = 1200
 CAL_SIZE = int(math.sqrt(CAL_AREA))
 CAL_UP = FRAME_CY + (CAL_SIZE / 2)
 CAL_LO = FRAME_CY - (CAL_SIZE / 2)
@@ -32,13 +32,12 @@ def main():
     # adjust camera settings
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, FRAME_X)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, FRAME_Y)
-    # cap.set(cv2.CAP_PROP_FPS, 30)
     cap.set(cv2.CAP_PROP_EXPOSURE, -8.0)
 
     while 687:
         ret, frame = cap.read()
 
-        cv2.rectangle(frame, CAL_UL, CAL_LR, (0, 255, 0), thickness=1)
+        cv2.rectangle(frame, CAL_UL, CAL_LR, (255, 0, 0), thickness=2)
         roi = frame[CAL_LO:CAL_UP, CAL_R:CAL_L]
         average_color_per_row = np.average(roi, axis=0)
         average_color = np.average(average_color_per_row, axis=0)
